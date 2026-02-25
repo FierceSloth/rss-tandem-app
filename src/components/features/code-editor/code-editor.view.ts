f7d808895bb94be9146b688011d27c120e24e8b7
Dastan Hairushev <hairushevdastan@gmail.com>
2026-02-25 20:20:10 +0500

    feat: implement value methods, readonly and ts props for code editor

--- src/components/features/code-editor/code-editor.view.ts
+++ src/components/features/code-editor/code-editor.view.ts
@@ -12,24 +12,49 @@ import { editorSetup } from './code-editor-theme';
 
 interface IProps extends IComponentChild {
   initialCode?: string;
+  typeScript?: boolean;
+  readOnly?: boolean;
 }
 
 export class CodeEditor extends Component {
   private view: EditorView;
 
-  constructor({ className = [], initialCode = '' }: IProps, ...children: Component[]) {
+  constructor(
+    { className = [], initialCode = '', typeScript = true, readOnly = false }: IProps,
+    ...children: Component[]
+  ) {
     const cssClasses = mergeClassNames(styles.codeEditor, className);
     super({ className: cssClasses }, ...children);
 
+    const editorExtensions = [basicSetup, javascript({ typescript: typeScript }), editorSetup];
+
+    if (readOnly) {
+      editorExtensions.push(EditorState.readOnly.of(true), EditorView.editable.of(false));
+    }
+
     const state = EditorState.create({
       doc: initialCode,
-      extensions: [basicSetup, javascript(), editorSetup],
+      extensions: editorExtensions,
     });
 
     this.view = new EditorView({
       state,
       parent: this.node,
     });
+  }
+
+  public setValue(newCode: string): void {
+    this.view.dispatch({
+      changes: {
+        from: 0,
+        to: this.view.state.doc.length,
+        insert: newCode,
+      },
+    });
+  }
+
+  public getValue(): string {
+    return this.view.state.doc.toString();
   }
 
   public destroy(): void {
--- src/pages/landing-page/landing-page.ts
+++ src/pages/landing-page/landing-page.ts
@@ -2,9 +2,60 @@ import styles from './landing-page.module.scss';
 import type { IPage } from '@/common/types/types';
 import { Component } from '@/components/base/component';
 
+import { CodeEditor } from '../../components/features/code-editor/code-editor.view';
+
+const initialCode = `import type { IComponentChild } from '@common/types/types';
+import { mergeClassNames } from '@common/utils/class-names';
+import { Component } from '@components/base/component';
+
+import styles from './test-widget.module.scss';
+
+interface IProps extends IComponentChild {
+  title?: string;
+  count?: number;
+}
+
+export class TestWidget extends Component {
+  private counter: number;
+
+  constructor(
+    { className = [], title = 'Default title', count = 0 }: IProps,
+    ...children: Component[]
+  ) {
+    const cssClasses = mergeClassNames(styles.widget, className);
+    super({ className: cssClasses }, ...children);
+
+    this.counter = count;
+    this.render(title);
+  }
+
+  private render(title: string): void {
+    this.node.textContent = \`\${title}: \${this.counter}\`;
+  }
+
+  public increment(): void {
+    this.counter += 1;
+    this.update();
+  }
+
+  private update(): void {
+    this.node.textContent = \`Count = \${this.counter}\`;
+  }
+
+  public destroy(): void {
+    console.log('Widget destroyed');
+    super.destroy();
+  }
+}`;
+
 export class LandingPage implements IPage {
   public render(): Component {
     const landing: Component = new Component({ className: [styles.landing, 'pageContainer'] });
+    const codeEditor = new CodeEditor({
+      initialCode: initialCode,
+    });
+    landing.append(codeEditor);
+
     return landing;
   }
 
