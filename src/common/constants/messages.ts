a1ac61c36c2b6ba087b283199ed72dc932bb312c
Anna <dilmun1101@gmail.com>
2026-03-02 17:04:29 +0500

    feat: RSS-TD-09 implement supabase (#19)
    - Install supabase
    - Аdd env file and interfaces for it in vite-env.d.ts
    - Add api connection of supabase
    - Add error message to constant

--- src/common/constants/messages.ts
+++ src/common/constants/messages.ts
@@ -5,6 +5,7 @@ export const messages = {
   errors: {
     urlQueryCorrupted: (url: string): string => `URL ${url} has more than 1 ?query sign`,
     pageNotFound: (page: string): string => `Page ${page} not found`,
+    missingSupabaseEnv: 'Missing Supabase environment variables',
   },
   terminal: {
     ready: 'System ready. Awaiting execution...',
