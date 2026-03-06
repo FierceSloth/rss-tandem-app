51cd53055b622815c0582e89f3f08b2cef634d17
Dastan Hairushev <hairushevdastan@gmail.com>
2026-03-06 19:53:15 +0500

    refactor: remove minor style duplication and fix typos

--- src/pages/video-task-resolver-page/video-task-resolver-page.ts
+++ src/pages/video-task-resolver-page/video-task-resolver-page.ts
@@ -5,7 +5,7 @@ import { PageLayout } from '@/components/layout/page-layout/page-layout.view';
 
 export class VideoTaskResolverPage implements IPage {
   public render(): Component {
-    const root = new PageLayout({ className: styles.vieoTaskResolver, withSidebar: false });
+    const root = new PageLayout({ className: styles.videoTaskResolver, withSidebar: false });
     return root;
   }
 
