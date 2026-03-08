b9113e8f0bd45114348921b5982bf6ade82e6859
Dastan Hairushev <hairushevdastan@gmail.com>
2026-03-08 20:42:37 +0500

    feat: implement roadmap header component

--- src/pages/roadmap-page/roadmap-page.ts
+++ src/pages/roadmap-page/roadmap-page.ts
@@ -2,10 +2,13 @@ import styles from './roadmap-page.module.scss';
 import type { IPage } from '@/common/types/types';
 import type { Component } from '@/components/base/component';
 import { PageLayout } from '@/components/layout/page-layout/page-layout.view';
+import { RoadmapHeader } from './components/layout/roadmap-header/roadmap-header.view';
 
 export class RoadmapPage implements IPage {
   public render(): Component {
-    const root = new PageLayout({ className: styles.roadmap, withSidebar: true });
+    const header = new RoadmapHeader({});
+
+    const root = new PageLayout({ className: styles.roadmap, withSidebar: true, header });
     return root;
   }
 
