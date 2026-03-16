7e6d4c40609d105caac665b05c1ef6eddc749863
Anna <dilmun1101@gmail.com>
2026-03-17 00:08:36 +0500

    feat: RSS-TD-10 add registration page (#44)
    - add view of login-form and register-form
    - add validator and controllers to registor and login forms
    - add registration to Supabase

--- src/pages/auth-page/auth-page.ts
+++ src/pages/auth-page/auth-page.ts
@@ -3,9 +3,17 @@ import type { IPage } from '@/common/types/types';
 import type { Component } from '@/components/base/component';
 import { PageLayout } from '@/components/layout/page-layout/page-layout.view';
 
+import { Modal } from './components/features/modal/modal.view';
+import { ModalController } from './components/features/modal/modal.controller';
+
 export class AuthPage implements IPage {
   public render(): Component {
     const root = new PageLayout({ className: styles.auth, withSidebar: false });
+
+    const modal = new Modal({});
+    new ModalController(modal);
+
+    root.append(modal);
     return root;
   }
 
