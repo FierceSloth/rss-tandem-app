61e670df1ba0c43005884ae056e97ac07704eca0
Dastan Hairushev <hairushevdastan@gmail.com>
2026-03-27 20:55:21 +0500

    feat: implement theory hub header component

---
+++ src/pages/theory-hub-page/common/constants/messages.ts
@@ -0,0 +1,6 @@
+export const messages = {
+  header: {
+    title: 'Theory Hub',
+    subTitle: 'Knowledge Base // Preparation',
+  },
+};
--- src/pages/theory-hub-page/theory-hub-page.ts
+++ src/pages/theory-hub-page/theory-hub-page.ts
@@ -2,10 +2,12 @@ import styles from './theory-hub-page.module.scss';
 import type { IPage } from '@/common/types/types';
 import type { Component } from '@/components/base/component';
 import { PageLayout } from '@/components/layout/page-layout/page-layout.view';
+import { TheoryHubHeader } from './components/layout/theory-hub-header/theory-hub-header.view';
 
 export class TheoryHubPage implements IPage {
   public render(): Component {
-    const root = new PageLayout({ className: styles.theoryHub, withSidebar: false });
+    const header = new TheoryHubHeader({});
+    const root = new PageLayout({ className: styles.theoryHub, withSidebar: false, header });
     return root;
   }
 
