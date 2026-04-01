ca1df8460b933fc586b09e678ce1206c88a80ce2
Dastan Hairushev <hairushevdastan@gmail.com>
2026-04-01 21:21:07 +0500

    feat: implement action buttons to theory hub

--- src/pages/theory-hub-page/theory-hub-page.controller.ts
+++ src/pages/theory-hub-page/theory-hub-page.controller.ts
@@ -4,6 +4,7 @@ import { theoryHubRepository } from './repositories/theory-hub.repository';
 import { ROUTES } from '@/router/constants';
 import { Toast } from '@/components/ui/toast/toast.view';
 import { messages } from '@/common/constants/messages';
+import { ProgressService } from '@/service/progress/progress.service';
 
 export class TheoryHubController {
   private view: TheoryHubPage;
@@ -52,7 +53,21 @@ export class TheoryHubController {
     });
 
     this.view.continueButton.addListener('click', () => {
-      this.navigate(ROUTES.ROADMAP_PAGE);
+      void (async (): Promise<void> => {
+        this.view.continueButton.setDisabled(true);
+        try {
+          await ProgressService.saveLevelProgress(this.levelId, 1, 1);
+          this.navigate(ROUTES.ROADMAP_PAGE);
+        } catch (error) {
+          if (error instanceof Error) {
+            new Toast({
+              message: error.message,
+              type: 'error',
+            });
+          }
+          this.view.continueButton.setDisabled(false);
+        }
+      })();
     });
   }
 }
